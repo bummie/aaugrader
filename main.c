dfeeaf3ed02a4dbe563ba7ee770bfd5d7b882d43
bummie <seebzei@gmail.com>
2025-03-17 18:15:53 +0100

    add parsing of grades
    now every line in grades.txt are split on semicolon.
    Only the username provided will be printed.

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int retrieveGrades(char *username){
+
+void parseGrades(char *username, char *input){
+    int userFound = 0;
+
+    char *token = strtok(input, ":");
+    while (token != NULL) {
+        if(!userFound && strcmp(token, username) == 0) {
+            userFound = 1;
+        }
+
+        if(userFound){
+          printf("%s\n", token);
+        }
+
+        token = strtok(NULL, ":");
+    }
+}
+
+// Takes username as input
+// Reads users grades from grades.txt
+void retrieveGrades(char *username){
   FILE *fp;
-  char output[1035];
+  char output[2035];
 
   char shell[] = "/bin/sh -c";
   char command[] = "cat";
@@ -17,22 +38,20 @@ int retrieveGrades(char *username){
     exit(1);
   }
 
-  while (fgets(output, sizeof(output), fp) != NULL) {
-    printf("%s", output);
+  while(fgets(output, sizeof(output), fp) != NULL) {
+    parseGrades(username, output);
   }
 
   pclose(fp);
-
-  return 0;
 }
 
 int main(int argc, char* argv[]) {
-  printf( " ▗▄▖  ▗▄▖ ▗▖ ▗▖         ▗▄▄▖▗▄▄▖  ▗▄▖ ▗▄▄▄  ▗▄▄▄▖▗▄▄▖ \n");
-  printf( "▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌        ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌  █ ▐▌   ▐▌ ▐▌\n");
-  printf( "▐▛▀▜▌▐▛▀▜▌▐▌ ▐▌        ▐▌▝▜▌▐▛▀▚▖▐▛▀▜▌▐▌  █ ▐▛▀▀▘▐▛▀▚▖\n");
-  printf( "▐▌ ▐▌▐▌ ▐▌▝▚▄▞▘        ▝▚▄▞▘▐▌ ▐▌▐▌ ▐▌▐▙▄▄▀ ▐▙▄▄▖▐▌ ▐▌\n");
-  printf( "                                                      \n");
-  printf( "(C) 2025                                              \n");
+  printf( " ▗▄▖  ▗▄▖ ▗▖ ▗▖    ▗▄▄▖▗▄▄▖  ▗▄▖ ▗▄▄▄  ▗▄▄▄▖▗▄▄▖ \n");
+  printf( "▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌   ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌  █ ▐▌   ▐▌ ▐▌\n");
+  printf( "▐▛▀▜▌▐▛▀▜▌▐▌ ▐▌   ▐▌▝▜▌▐▛▀▚▖▐▛▀▜▌▐▌  █ ▐▛▀▀▘▐▛▀▚▖\n");
+  printf( "▐▌ ▐▌▐▌ ▐▌▝▚▄▞▘   ▝▚▄▞▘▐▌ ▐▌▐▌ ▐▌▐▙▄▄▀ ▐▙▄▄▖▐▌ ▐▌\n");
+  printf( "                                                 \n");
+  printf( "(C) 2025                                         \n");
 
   printf("This program returns the grades for the specified user.\n");
 
