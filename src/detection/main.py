c56bc86ae44612b5e5a8b31c4bd3a53c9fef44cc
bummie <seebzei@gmail.com>
2025-04-01 17:38:16 +0200

    refac: move syscall parser out to its own file

--- src/detection/main.py
+++ src/detection/main.py
@@ -1,69 +1,10 @@
 #!/usr/bin/env python3
 import argparse
-import json
-import re
 import sys
 
 import rustworkx as rx
+import syscallparser as sp
 from rustworkx.visualization import graphviz_draw
-
-
-class Syscall:
-    def __init__(self, pid: int, syscall: str, args: list[str], result: str, raw: str):
-        self.pid = pid
-        self.syscall = syscall
-        self.args = args
-        self.result = result
-        self.raw = raw
-
-    def to_json(self) -> str:
-        return json.dumps(self.__dict__)
-
-
-def regex_match_syscall(line: str) -> list:
-    return re.findall(r"(\w+)\((.*)\)\s+=\s+(0x[0-9a-fA-F]+|-?\d+)", line)
-
-
-def parse_strace_output(input_line: str) -> Syscall:
-    # Regex that matches strace output
-    # read(3</home/bummie/projects/AAUGrader/grades.txt>, "", 262144) = 0
-    # SYSCALL: read
-    # Syscall args: 3</home/bummie/projects/AAUGrader/grades.txt>, "", 262144
-    # Output syscall: 0
-
-    # Process might spawn babies
-    # Syscall wills tart with their PID then
-    # [pid 122345] read(args) = 1
-
-    pid = -1
-    matches = None
-
-    if input_line.lower().startswith("[pid"):
-        pid = int(input_line.split("[pid ")[1].split("]")[0])
-        strace_output_no_pid = "".join(input_line.split("]")[1:])
-        matches = regex_match_syscall(strace_output_no_pid)
-    elif input_line.lower().startswith("<..."):
-        raise ValueError(f"unwanted line: {input_line}")
-    else:
-        matches = regex_match_syscall(input_line)
-
-    if len(matches) <= 0:
-        # Found no matches
-        raise ValueError("found no matches")
-
-    if len(matches[0]) == 3:
-        syscall = matches[0][0]
-        args = matches[0][1].split(", ")
-        result = matches[0][2]
-
-        return Syscall(pid, syscall, args, result, input_line)
-
-    elif len(matches[0]) == 2:
-        syscall = matches[0][0]
-        result = matches[0][1]
-        return Syscall(pid, syscall, [], result, input_line)
-    else:
-        raise ValueError(f"failed parsing regex matches {matches}")
 
 
 def int_to_color_str(index: int) -> str:
@@ -78,17 +19,17 @@ def int_to_color_str(index: int) -> str:
     return colors[index % len(colors)]
 
 
-def node_attr(node: Syscall) -> str:
+def node_attr(node: sp.Syscall) -> str:
     return {"color": int_to_color_str(node.pid), "label": node.syscall}
 
 
-def parse_stdin_to_syscalls() -> list[Syscall]:
-    syscalls: list[Syscall] = []
+def parse_stdin_to_syscalls() -> list[sp.Syscall]:
+    syscalls: list[sp.Syscall] = []
 
     for line in sys.stdin:
         syscall = None
         try:
-            syscall = parse_strace_output(line)
+            syscall = sp.parse_strace_output(line)
             syscalls.append(syscall)
         except Exception as err:
             print(f"failed parsing line: {err}\n{line}", file=sys.stderr)
@@ -97,7 +38,7 @@ def parse_stdin_to_syscalls() -> list[Syscall]:
     return syscalls
 
 
-def build_graph(syscalls: list[Syscall]) -> rx.PyDiGraph:
+def build_graph(syscalls: list[sp.Syscall]) -> rx.PyDiGraph:
     graph = rx.PyDiGraph()
     prev_pid_nodes = {}
     prev_node = None
@@ -124,10 +65,8 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    syscalls: list[Syscall] = parse_stdin_to_syscalls()
+    syscalls: list[sp.Syscall] = parse_stdin_to_syscalls()
+    sp.syscalls_to_jsonl(syscalls, f"/tmp/{args.output}.jsonl")
 
     graph = build_graph(syscalls)
-    rx.digraph_node_link_json(graph, path=f"/tmp/{args.output}.json")
-    graphviz_draw(
-        graph, node_attr_fn=node_attr, filename=f"/tmp/{args.output}.png", method="dot"
-    )
+    graphviz_draw(graph, node_attr_fn=node_attr, filename=f"/tmp/{args.output}.png")
