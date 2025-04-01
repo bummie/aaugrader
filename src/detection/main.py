941db0c0f4bd1e211a49a9ecb9ff4fb332d4584d
bummie <seebzei@gmail.com>
2025-04-01 17:44:26 +0200

    refac: move graphing code into own file

--- src/detection/main.py
+++ src/detection/main.py
@@ -2,25 +2,7 @@
 import argparse
 import sys
 
-import rustworkx as rx
 import syscallparser as sp
-from rustworkx.visualization import graphviz_draw
-
-
-def int_to_color_str(index: int) -> str:
-    colors = [
-        "gold",
-        "lightskyblue",
-        "lightsalmon",
-        "lightgreen",
-        "lightcoral",
-        "lightcoral",
-    ]
-    return colors[index % len(colors)]
-
-
-def node_attr(node: sp.Syscall) -> str:
-    return {"color": int_to_color_str(node.pid), "label": node.syscall}
 
 
 def parse_stdin_to_syscalls() -> list[sp.Syscall]:
@@ -38,26 +20,6 @@ def parse_stdin_to_syscalls() -> list[sp.Syscall]:
     return syscalls
 
 
-def build_graph(syscalls: list[sp.Syscall]) -> rx.PyDiGraph:
-    graph = rx.PyDiGraph()
-    prev_pid_nodes = {}
-    prev_node = None
-
-    for syscall in syscalls:
-        print(syscall.to_json())
-
-        node_index = graph.add_node(syscall)
-        if syscall.pid in prev_pid_nodes:
-            graph.add_edge(prev_pid_nodes[syscall.pid], node_index, None)
-        elif prev_node is not None:
-            graph.add_edge(prev_node, node_index, None)
-
-        prev_pid_nodes[syscall.pid] = node_index
-        prev_node = node_index
-
-    return graph
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Detection graph builder 1337")
     parser.add_argument(
@@ -67,6 +29,3 @@ if __name__ == "__main__":
 
     syscalls: list[sp.Syscall] = parse_stdin_to_syscalls()
     sp.syscalls_to_jsonl(syscalls, f"/tmp/{args.output}.jsonl")
-
-    graph = build_graph(syscalls)
-    graphviz_draw(graph, node_attr_fn=node_attr, filename=f"/tmp/{args.output}.png")
