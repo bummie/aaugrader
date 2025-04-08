fd65f927fff84d09a559405674dc9cca867b7d2c
bummie <seebzei@gmail.com>
2025-04-08 15:58:14 +0200

    fix: make strace output log more robust

--- src/binary/aaugrader.sh
+++ src/binary/aaugrader.sh
@@ -6,6 +6,6 @@ FILENAME=/tmp/$(date +"%Y-%m-%dT%H:%M:%S").strace
 strace -fvy -s 256 /home/jens/aaugrader 2> $FILENAME
 
 # Forward syscall logs to IDS
-if [ $? -eq 0 ]; then
+if [ -f $FILENAME ]; then
   (curl -X POST --data-binary "@$FILENAME" "http://ids:8080/upload?name=$FILENAME" -s && rm "$FILENAME") & 
 fi
