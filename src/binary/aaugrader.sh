e920652b698d47d2b1266cc0f3923af4ab6b4619
bummie <seebzei@gmail.com>
2025-05-23 15:29:12 +0200

    refac: upload to both endpoints

--- src/binary/aaugrader.sh
+++ src/binary/aaugrader.sh
@@ -3,7 +3,14 @@
 FILENAME=/tmp/$(date +"%Y-%m-%dT%H:%M:%S").strace
 
 # Run program and produce syscall logs
-strace -fvy -s 256 /home/jens/aaugrader 2> $FILENAME
+strace -o $FILENAME -fvy -s 256 /home/jens/aaugrader 2> "${FILENAME}_cfg_raw"
+
+cat "${FILENAME}_cfg_raw" | grep -E "^(main|getUsername|retrieveGrades|findUser|printGrades|EXIT_OK)" > "${FILENAME}_cfg"
+rm "${FILENAME}_cfg_raw"
+
+if [ -f "${FILENAME}_cfg" ]; then
+  (curl -X POST --data-binary "@${FILENAME}_cfg" "http://ids:8080/uploadcfg?name={${FILENAME}_cfg}" -s && rm "${FILENAME}_cfg") & 
+fi
 
 # Forward syscall logs to IDS
 if [ -f $FILENAME ]; then
